fb0554fe9ff8caffea20d3cecde1dd1faa8811df
Aly “Stjousama” <73197677+alyilmaz99@users.noreply.github.com>
2022-03-14 14:04:51 +0000

    nothing

--- index.js
+++ index.js
@@ -1,26 +1,140 @@
+
 const express = require('express');
 const app = express();
 const port = 3000;
+const dennaList = require("./lists.js");
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId, token } = require('./config.json');
+
+//--------------------------------------------------------//
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
 
-// ================= START BOT CODE ===================
+// =================BURAYA BAKARLAR =================== //
 
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isCommand()) return;
-
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
+client.on('messageCreate', (message) =>{
+  if(message.content === 'ping'){
+    message.reply({
+      content: 'pong',
+    })
   }
 });
+client.on('message', function (message) {
+  if (message.content.split('').join('') === '!roll') {
+    return message.reply(`U got a ${Math.floor((Math.random() * 6) + 1)}`);
+   }
+});
+client.on('messageCreate', (message) =>{
+  if(message.content === 'Denna'){
+    message.reply({
+      content: 'Efendim',
+    })
+  }
+});client.on('messageCreate', (message) =>{
+  if(message.content === 'Naber'){
+    message.reply({
+      content: 'ii sn',
+    })
+  }
+});
+client.on('messageCreate', (message) =>{
+  var degisken = Math.floor((Math.random() * dennaList.dennaList().length) );
+  var list = dennaList.dennaList();
+  if(message.content === '!sendnudes'){
+    message.reply({
+      
+      content:list[degisken],
+    })
+  }
+});
+client.on('message', (message) =>{
+if(message.content === 'denna nasıl birisi'){
+  message.reply({
+    content: dennaList.dennaDesc(),
+  })
+}
+});
+client.on('message', (message) =>{
+if(message.content === 'medeniyetin dört köşesi'){
+  message.reply({
+    content: "https://i.redd.it/d4gu6qfvnd961.png",
+  })
+}
+});
+client.on('message', (message) =>{
+if(message.content === 'four corners of civilization'){
+  message.reply({
+    content: "https://i.redd.it/d4gu6qfvnd961.png",
+  })
+}
+});
+client.on('message', (message) =>{
+if(message.content === 'denna ardaya söv'){
+  message.reply({
+    content: "aq ardası",
+  })
+}
+});
+client.on('message', (message) =>{
+if(message.content === 'ayıp'){
+  message.reply({
+    content: "ayıp yatakta olur",
+  })
+}
+});
+const commands = [
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+]
+	.map(command => command.toJSON());
 
-client.login('OTM5ODY5NzYxMjgwNTUyOTcw.Yf_H7Q.bk_7UJhCXXg_TGZICS7gsw-jIDw');
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	} else if (commandName === 'user') {
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	}
+});
+
+/*
+ void UpdatePresence()
+{
+    DiscordRichPresence discordPresence;
+    memset(&discordPresence, 0, sizeof(discordPresence));
+    discordPresence.state = "Yalnız Başına";
+    discordPresence.details = "Aşkı Arıyor";
+    discordPresence.startTimestamp = 1507665886;
+    discordPresence.endTimestamp = 1507665886;
+    discordPresence.largeImageText = "SAD";
+    discordPresence.smallImageText = "Rogue - Level 100";
+    discordPresence.partyId = "ae488379-351d-4a4f-ad32-2b9b01c91657";
+    discordPresence.partySize = 1;
+    discordPresence.partyMax = 2;
+    discordPresence.joinSecret = "MTI4NzM0OjFpMmhuZToxMjMxMjM= ";
+    Discord_UpdatePresence(&discordPresence);
+}*/
+client.login('OTM5ODY5NzYxMjgwNTUyOTcw.Yf_H7Q.lLKtjnPHDF209qwf7DEQOHd96NI');
---
+++ lists.js
@@ -0,0 +1,54 @@
+var dennaResim = ["https://static.wikia.nocookie.net/nameofthewind/images/5/5d/Denna_name_of_the_wind.png/revision/latest/scale-to-width-down/500?cb=20190509003613",
+"https://static.wikia.nocookie.net/nameofthewind/images/2/21/Denna_by_abey.png/revision/latest/scale-to-width-down/507?cb=20160804062703",
+"https://static.wikia.nocookie.net/nameofthewind/images/8/88/Denna_sketch_by_sydrianxoxo-d97ypsq.jpg.jpg/revision/latest/scale-to-width-down/900?cb=20160804060431",
+"https://static.wikia.nocookie.net/nameofthewind/images/b/ba/Da3v49d-ba4de87a-99c1-4c87-8db2-0e26e1edbe96.jpg/revision/latest/scale-to-width-down/1000?cb=20190509003843",
+"https://static.wikia.nocookie.net/nameofthewind/images/f/f6/Denna_kingkiller.png/revision/latest/scale-to-width-down/1000?cb=20170316020453",
+"https://static.wikia.nocookie.net/nameofthewind/images/0/04/Denna1.jpg/revision/latest/scale-to-width-down/700?cb=20160803124511",
+"https://static.wikia.nocookie.net/nameofthewind/images/8/89/Tumblr_p3wji64yjo1wbrmxbo1_640.jpg/revision/latest/scale-to-width-down/600?cb=20190509004801",
+"https://static.wikia.nocookie.net/nameofthewind/images/1/1e/Drakhas-oguzalp-donduren-denna3.jpg/revision/latest/scale-to-width-down/1000?cb=20180922145948",
+"https://static.wikia.nocookie.net/nameofthewind/images/b/bc/Denna-1.jpg/revision/latest/scale-to-width-down/299?cb=20140530184245",
+"https://static.wikia.nocookie.net/nameofthewind/images/5/5d/Dennacard.jpg/revision/latest/scale-to-width-down/457?cb=20180125095946",
+"https://static.wikia.nocookie.net/nameofthewind/images/1/11/Denna.png/revision/latest/scale-to-width-down/1000?cb=20220123192131",
+"https://static.wikia.nocookie.net/nameofthewind/images/c/c8/Denna.jpg/revision/latest/scale-to-width-down/337?cb=20170816215021",
+"https://upload.wikimedia.org/wikipedia/commons/c/cf/Kingkiller_denna_bynataconfressas.png",
+"https://pro2-bar-s3-cdn-cf2.myportfolio.com/280b9dab768bd0287d60886754533313/3f422bff-303e-4337-9c8f-89869c5b4aa9_rw_1920.jpg?h=f377543780558167c768ffcd7fc37aec",
+"https://i.pinimg.com/736x/97/8b/c4/978bc46668108c94b573357037d79a6a.jpg",
+"https://i.redd.it/xfjk5y32yjv21.jpg",
+"https://i.pinimg.com/736x/dc/0d/73/dc0d7332ce1e33735ffc52b917c5ff53--the-kingkiller-chronicles-fantasy-inspiration.jpg",
+"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/54ab5d2a-5d8f-4458-ba52-c92dccfddb98/d8id0ty-6d93f6e8-ebc6-44b2-be43-1427a65506db.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzU0YWI1ZDJhLTVkOGYtNDQ1OC1iYTUyLWM5MmRjY2ZkZGI5OFwvZDhpZDB0eS02ZDkzZjZlOC1lYmM2LTQ0YjItYmU0My0xNDI3YTY1NTA2ZGIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.h1nOVSIEY0YwcOF8XuX47QkFR2dk7__NyCOq3TEbGIU",
+"https://cdnb.artstation.com/p/assets/images/images/005/679/381/large/anna-de-cicco-dennapic.jpg?1492967712",
+"https://pbs.twimg.com/media/DaF2kyIV4AIINPm.jpg",
+"https://static.wikia.nocookie.net/nameofthewind/images/2/21/Denna_by_abey.png/revision/latest/scale-to-width-down/250?cb=20160804062703",
+"https://64.media.tumblr.com/e25f231ec9b7122f61f75eec930a0f9f/tumblr_inline_om9a9e0Knj1smektc_1280.jpg",];
+
+var description  = `The name of the wind, page 470 (Kvothe and Deoch):
+
+K: So you've known Denna for quite a while
+
+D: Off and on. More off, honestly.
+
+K: What was she like back then?
+
+D: The same. I suppose she was younger, but I can't say she seems any older now. She always struck me as being older than her years. Not old really, more...
+
+K: Mature?
+
+D: No, I don't know a good word for it. It's like if you look at a great oak tree. You don't appreciate it because it's older than the other trees, or because it's taller. It just has something that other younger trees don't. Complexity, solidity, significance. Damn if that isn't the worst comparison I've ever made.
+
+K: It's nice to see I'm not the only one who has trouble pinning her down with words.
+
+D: She's not much for being pinned down. D: She was just as restless then, and wild. Just as pretty, prone to startle the eye and stutter the heart.
+
+Shortly after the above (Deoch to Kvothe):
+
+D: No, I do not envy her life. Nor do I judge her. For all that, I would help her, if she would let me. But she’s not the sort to be beholden to anyone.`;
+
+module.exports = {
+        dennaList: () => {
+           return dennaResim;
+        },
+        dennaDesc: () =>{
+          return description;
+        }
+       
+    };
